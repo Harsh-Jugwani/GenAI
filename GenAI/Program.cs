a3353583cfcf8d3b7bdf4e3e88f9e12152ace792
Harsh.Jugwani <Harsh.Jugwani@mindruby.com>
2026-04-21 16:35:00 +0530

    Uses agent session for context.

--- GenAI/Program.cs
+++ GenAI/Program.cs
@@ -6,8 +6,22 @@ var chatClient = new OllamaChatClient(
     new Uri("http://20.20.20.186:11434/"),
     modelId: "llama3.2:3b");
 
+
 AIAgent agent = chatClient.AsAIAgent(
     instructions: "You are a helpful assistant running locally via Ollama.");
 
-Console.WriteLine(await agent.RunAsync("What is the largest city in France?"));
+AgentSession session = await agent.CreateSessionAsync();
+
+while (true)
+{
+    Console.Write("User: ");
+    string userInput = Console.ReadLine();
+    if (string.IsNullOrEmpty(userInput) || userInput.ToLower() == "exit")
+        break;
+    
+    await foreach(var update in agent.RunStreamingAsync(userInput, session))
+    {
+        Console.Write(update);
+    }
+}
 
